068bf2c556d3984c93ebda5bb698aa7f8b5d5b47
FUNco <crispy.alc@gmail.com>
2023-04-04 23:30:28 +0900

    class_2

--- class.dart
+++ class.dart
@@ -8,7 +8,25 @@ class Player {
   }
 }
 
+class Player2 {
+  String name;
+  int age;
+  String team;
+
+  Player2({
+    required this.name, // required keyword should be added cause this params don't allowed to be null
+    required this.age, // required keyword should be added cause this params don't allowed to be null
+    required this.team, // required keyword should be added cause this params don't allowed to be null
+  });
+  void sayHello() {
+    print("Hello my name is $name $age yars old in $team team");
+  }
+}
+
 void main() {
   var player = Player();
   player.sayHello();
+
+  var player2 = Player2(name: "Creang", team: "blue", age: 20);
+  player2.sayHello();
 }
